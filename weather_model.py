9149f3e90cbe9278daa64bc2d19b9b4e56ad857e
Daniel Vivor <you@example.com>
2026-06-14 19:10:28 +0200

    Add add_to_history method to write lookup records to memory

--- weather_model.py
+++ weather_model.py
@@ -3,3 +3,12 @@ class WeatherAppModel:
     def __init__(self):
         self.search_history = []
         self.favorites = set()
+
+def add_to_history(self, city, temperature, condition):
+        """Adds a successful query's key metrics to the history log."""
+        record = {
+            "city": city.title(),
+            "temp": temperature,
+            "condition": condition
+        }
+        self.search_history.append(record)
